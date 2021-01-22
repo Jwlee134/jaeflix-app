e53e9a8943ac13b2585da8e36808f6e49e7459cf
Jaewon <sorhd134@gmail.com>
2021-01-22 13:21:47 +0900

    New App Icon & Splash Img

--- android/app/src/main/java/com/movieapp/MainActivity.java
+++ android/app/src/main/java/com/movieapp/MainActivity.java
@@ -2,6 +2,8 @@ package com.movieapp;
 
 import android.os.Bundle;
 import org.devio.rn.splashscreen.SplashScreen;
+import android.os.Bundle;
+import org.devio.rn.splashscreen.SplashScreen;
 import com.facebook.react.ReactActivity;
 
 public class MainActivity extends ReactActivity {
